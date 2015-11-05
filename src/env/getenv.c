36f7c397ccd81c20ff77cd904bd94b032a734d26
gabriel d. karpman <gdkar@mit.edu>
2015-11-05 02:27:55 -0500

    tiny bit of setenv/putenv cleanup

--- src/env/getenv.c
+++ src/env/getenv.c
@@ -7,38 +7,30 @@
 char *getenv(const char *name)
 {
   if(!__environ || !name[0] ) return NULL;
-  uint16_t  * name_16 = (uint16_t*)name;
-  uint16_t ** environ_16 = (uint16_t**)__environ;
+  if(!__environ || !name[0] ) return NULL;
   if(!name[1])
   {
     uint16_t name_start = ('='<<8)|name[0];
-    for(uint16_t **ep = environ_16;*ep;++ep)
+    for(uint16_t **ep = (uint16_t**)__environ;*ep;++ep)
     {
         uint16_t ep_start = **ep;
         memmove(&ep_start,ep,sizeof(ep_start));
-        if ( name_start == ep_start )
-          return (char*)&(*ep)[1];
+        if ( name_start == ep_start ) return (char*)&(*ep)[1];
     }
   }
   else
   {
-    uint16_t name_start = name_16[0];
-    name = (const char *)&name_16[1];
+    uint16_t name_start = *(uint16_t*)name;
+    name = &name[2];
     size_t len = strlen(name);
-    for(uint16_t **ep = environ_16; *ep; ++ep)
+    for(char **ep = __environ; *ep; ++ep)
     {
-      uint16_t ep_start = **ep;
-      if ( ep_start == name_start && !strncmp ( (const char*)&(*ep)[1],name,len)
+      if ( !(*ep)[2] ) continue;
+      uint16_t ep_start = *(uint16_t*)(*ep);
+      if ( ep_start == name_start && !strncmp ( &(*ep)[2],name,len)
                                   && (*ep)[len + 2] == '=')
-        return &((char*)*ep)[len+3];
+        return &(*ep)[len+3];
     }
   }
   return NULL;
-/*	int i;
-	size_t l = strlen(name);
-	if (!__environ || !*name || strchr(name, '=')) return NULL;
-	for (i=0; __environ[i] && (strncmp(name, __environ[i], l)
-		|| __environ[i][l] != '='); i++);
-	if (__environ[i]) return __environ[i] + l+1;
-	return NULL;*/
 }
--- src/env/setenv.c
+++ src/env/setenv.c
@@ -2,14 +2,15 @@
 #include <string.h>
 #include <errno.h>
 
-int __putenv(char *s, int a);
+int __setenv(char *s);
 
 int setenv(const char *var, const char *value, int overwrite)
 {
 	char *s;
 	int l1, l2;
 
-	if (!var || !*var || strchr(var, '=')) {
+	if (!var || !*var || strchr(var, '='))
+  {
 		errno = EINVAL;
 		return -1;
 	}
@@ -23,7 +24,7 @@ int setenv(const char *var, const char *value, int overwrite)
 		s[l1] = '=';
 		memcpy(s+l1+1, value, l2);
 		s[l1+l2+1] = 0;
-		if (!__putenv(s, 1)) return 0;
+		if (!__setenv(s)) return 0;
 	}
 	free(s);
 	return -1;
